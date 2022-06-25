1e53f7e2fe9c9198cfeb8cedfb2278427faee769
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-06-25 16:42:14 +0800

    feat: add create todo function

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../../models/category')
+const Record = require('../../models/record')
 
+router.get('/new', (req, res) => {
+  res.render('new')
+})
 
+router.post('/', (req, res) => {
+  const { name, category, date, amount } = req.body
+  const userId = req.user._id
+  Category.findOne({ category })
+    .lean()
+    .then(category => {
+      return Record.create({
+        name,
+        date,
+        amount,
+        userId,
+        categoryId: category._id
+      })
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
 
 module.exports = router
