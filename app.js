73b2fa83a2c0ab7213a88ed853991088923ed43d
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-06-25 14:29:45 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const routes = require('./routes')
 
 const mongoose = require('mongoose')
@@ -23,6 +24,12 @@ const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(express.urlencoded({ extended: true })) 
 
