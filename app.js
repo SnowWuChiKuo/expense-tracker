432a5cfd712627317bcc1037d356f388fe6a54c9
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-06-25 15:16:33 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const session = require('express-session')
 const routes = require('./routes')
 const usePassport = require('./config/passport')
 const mongoose = require('mongoose')
+const flash = require('connect-flash')
+
 const MONGODB_URI = "mongodb://localhost/expense-tracker"
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -33,10 +35,12 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: true })) 
 usePassport(app)
-
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const Record = require('../../models/record')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  const userId = req.user._id
+  Record.find({ userId })
+    .populate('categoryId') // mongoose populate:包含
+    .lean()
+    .then(records => {
+      let totalAmount = 0
+      records.map(record => {
+        totalAmount += Number(record.amount)
+      })
+      return res.render('index', { records, totalAmount })
+    })
 })
 
 module.exports = router
