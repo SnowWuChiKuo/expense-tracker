63ef05a64ad841665cb2e648d31252a1358be516
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-06-25 17:18:32 +0800

    feat: add mongoose & categorySeeder

--- app.js
+++ app.js
@@ -1,28 +1,15 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const session = require('express-session')
 const routes = require('./routes')
+const session = require('express-session')
 const usePassport = require('./config/passport')
-const mongoose = require('mongoose')
 const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
-// 取得連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
+require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT
