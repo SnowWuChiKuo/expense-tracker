5a05c33fb00023a22d0eabff4c3cf2ad0888b41c
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-06-26 10:03:36 +0800

    feat: modify category route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
   const userId = req.user._id
@@ -13,6 +14,32 @@ router.get('/', (req, res) => {
         totalAmount += Number(record.amount)
       })
       return res.render('index', { records, totalAmount })
+    })
+})
+
+router.post('/category', (req, res) => {
+  const reqCategory = req.body.category
+  const userId = req.user._id
+  Category.find()
+    .lean()
+    .then(category => {
+      const recordCategory = []
+      const otherCategory = []
+      category.map(category => {
+        if (category.name === reqCategory) {
+          return recordCategory.push(category)
+        } else {
+          return otherCategory.push(category)
+        }
+      })
+      Record.find({ userId, categoryId: recordCategory[0]._id })
+        .populate('categoryId')
+        .lean()
+        .then(records => {
+          let totalAmount = 0
+          records.map(record => totalAmount += Number(record.amount))
+          res.render('index', { records, totalAmount, recordCategory: recordCategory[0], otherCategory })
+        })
     })
 })
 
